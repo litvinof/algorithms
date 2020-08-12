f95c9e30d2e5c6e4614ba38b057d082e3267f9a8
Viacheslav Litvinov <viacheslavlitvinov@gmail.com>
2020-08-12 15:23:09 +0700

    (python) Insertion sort

---
+++ python/insertion_sort.py
@@ -0,0 +1,14 @@
+def insertion_sort(unsorted_arr):
+    for idx in range(1, len(unsorted_arr)):
+        # Bubble down the taken element
+        sub_idx = idx
+        while sub_idx > 0 and unsorted_arr[sub_idx] < unsorted_arr[sub_idx - 1]:
+            # Swap
+            unsorted_arr[sub_idx], unsorted_arr[sub_idx - 1] = unsorted_arr[sub_idx - 1], unsorted_arr[sub_idx]
+            sub_idx -= 1
+    return unsorted_arr
+
+
+test_arr = [4, 3, 7, 193, 1, 6, 2]
+
+print(insertion_sort(test_arr))
